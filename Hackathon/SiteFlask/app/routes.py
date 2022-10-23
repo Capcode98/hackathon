106d803c11d87da6d49dd12d51de0dfa6359eeea
João Luis B. Ribeiro <89806860+Capcode98@users.noreply.github.com>
2022-10-23 06:00:42 -0300

    commit

--- Hackathon/SiteFlask/app/routes.py
+++ Hackathon/SiteFlask/app/routes.py
@@ -1,3 +1,4 @@
+import mysql.connector
 from app import app
 from flask import render_template, request
 
@@ -21,8 +22,36 @@ def error(nome):
 def autenticar():
     usuario = request.form.get('usuario')
     senha = request.form.get('senha')
-    return f'usuario: {usuario} e senha: {senha}'
+    login(usuario, senha)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+def login(usuario, senha):
+    # Connect to server
+    cnx = mysql.connector.connect(
+        host="127.0.0.1",
+        port=3306,
+        user="root",
+        password="123456",
+        database="dbteste")
+
+    # Get a cursor
+    cur = cnx.cursor()
+
+    # Execute a query
+    cur.execute("SELECT * FROM usuario WHERE nome = %s and senha = %s", (usuario, senha))
+
+    myresult = cur.fetchone()
+
+
+    # Close connection
+    cnx.close()
+
+    return myresult
+
+result = login('Diego', '123456')
+
+print(result)
